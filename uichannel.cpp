037a1db2fdbe89941b9674c4793a413319badb77
Gauthier Legrand <gotleg@gmail.com>
2013-01-10 18:53:27 +0100

    New UI widget to manipulate MinoChannel (and related animations)

---
+++ uichannel.cpp
@@ -0,0 +1,67 @@
+#include "uichannel.h"
+#include "uianimation.h"
+
+#include "minoanimation.h"
+#include "uipixeledview.h"
+
+#include <QFrame>
+#include <QScrollArea>
+#include <QHBoxLayout>
+#include <QDebug>
+
+UiChannel::UiChannel(MinoChannel *channel, QWidget *parent) :
+    QWidget(parent),
+    _channel(channel)
+{
+    QVBoxLayout *layout = new QVBoxLayout(this);
+
+    // Preview
+    QFrame *fPreview = new QFrame(this);
+    fPreview->setFrameShape(QFrame::Box);
+    fPreview->setSizePolicy(QSizePolicy::Minimum,QSizePolicy::Minimum);
+    fPreview->setMinimumSize(250, 170);
+    layout->addWidget(fPreview);
+
+    QVBoxLayout *lPreview = new QVBoxLayout(fPreview);
+    UiPixeledView *preview = new UiPixeledView(channel->view(), fPreview);
+    QSizePolicy policy(QSizePolicy::Minimum,QSizePolicy::Minimum);
+    policy.setHeightForWidth(true);
+    preview->setSizePolicy(policy);
+    preview->setMinimumSize(240, 160);
+    lPreview->addWidget(preview);
+    connect(channel, SIGNAL(animated()), preview, SLOT(refresh()));
+
+    // Animations properties
+    QScrollArea *sa = new QScrollArea();
+    layout->addWidget(sa);
+
+    QWidget *wContent = new QWidget(this);
+    sa->setWidget(wContent);
+    sa->setWidgetResizable(true);
+
+    QHBoxLayout *lContent = new QHBoxLayout(wContent);
+
+    foreach (MinoAnimation *animation, _channel->animations())
+    {
+        UiAnimation *uiAnimation = new UiAnimation(animation, wContent);
+        connect(uiAnimation, SIGNAL(customContextMenuRequested(QPoint)), this, SIGNAL(customContextMenuRequested(QPoint)));
+
+        lContent->addWidget(uiAnimation);
+
+        QFrame *frame = new QFrame(wContent);
+        frame->setFrameShape(QFrame::VLine);
+        lContent->addWidget(frame);
+    }
+}
+
+UiChannel::~UiChannel()
+{
+
+}
+
+
+/*
+QAction * UiChannel::midiCaptureAction()
+{
+  return _midiCaptureAction;
+}*/
