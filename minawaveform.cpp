b225c383e97a4d4cf4bd4572a2e915fa667fe403
Gauthier Legrand <gotleg@gmail.com>
2013-02-09 17:55:58 +0100

    gradient test on weveform animation

--- minawaveform.cpp
+++ minawaveform.cpp
@@ -23,6 +23,15 @@ void MinaWaveform::animate(const unsigned int uppqn, const unsigned int gppqn, c
     QColor color;
     color.setHsvF(_color.value(), 1.0, 1.0);
 
+    QLinearGradient
+       grad(0.0, 0.0, 0.0, 16.0) ;
+    grad.setColorAt(0.0, Qt::red) ;
+    grad.setColorAt(0.3, Qt::yellow) ;
+    grad.setColorAt(0.45, Qt::green) ;
+    grad.setColorAt(0.55, Qt::green) ;
+    grad.setColorAt(0.7, Qt::yellow) ;
+    grad.setColorAt(1, Qt::red) ;
+
     const qreal middle = (qreal)_boundingRect.height()/2;
     foreach(QGraphicsItem* item, _itemGroup.childItems ())
     {
@@ -31,6 +40,6 @@ void MinaWaveform::animate(const unsigned int uppqn, const unsigned int gppqn, c
     for (int i=0; i<_boundingRect.width(); i++)
     {
         qreal randHeight = qrandF() * _beatAnimatedProperty.currentValue().toReal();
-        _itemGroup.addToGroup(_scene->addLine(i, middle-randHeight, i, middle+randHeight, QPen(color)));
+        _itemGroup.addToGroup(_scene->addLine(i, middle-randHeight, i, middle+randHeight, QPen(QBrush(grad),1)));
     }
 }
