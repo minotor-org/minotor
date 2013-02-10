e91f47567c75af274e49cabdad0bfedc432a2822
Gauthier Legrand <gotleg@gmail.com>
2013-02-10 03:37:00 +0100

    Waveform animation now generates a dynamical gradient using the selected color

--- minawaveform.cpp
+++ minawaveform.cpp
@@ -22,15 +22,22 @@ void MinaWaveform::animate(const unsigned int uppqn, const unsigned int gppqn, c
 
     QColor color;
     color.setHsvF(_color.value(), 1.0, 1.0);
+    qreal maxValue = (_color.value()<0.8)?_color.value()+0.2:1.0;
+    qreal minValue = (_color.value()>0.2)?_color.value()-0.2:0.0;
+    QColor colorMax;
+    QColor colorMin;
+
+    colorMax.setHsvF(maxValue, 1.0, 1.0);
+    colorMin.setHsvF(minValue, 1.0, 1.0);
 
     QLinearGradient
        grad(0.0, 0.0, 0.0, 16.0) ;
-    grad.setColorAt(0.0, Qt::red) ;
-    grad.setColorAt(0.3, Qt::yellow) ;
-    grad.setColorAt(0.45, Qt::green) ;
-    grad.setColorAt(0.55, Qt::green) ;
-    grad.setColorAt(0.7, Qt::yellow) ;
-    grad.setColorAt(1, Qt::red) ;
+    grad.setColorAt(0.0, colorMin) ;
+    grad.setColorAt(0.3, color) ;
+    grad.setColorAt(0.45, colorMax) ;
+    grad.setColorAt(0.55, colorMax) ;
+    grad.setColorAt(0.7, color) ;
+    grad.setColorAt(1, colorMin) ;
 
     const qreal middle = (qreal)_boundingRect.height()/2;
     foreach(QGraphicsItem* item, _itemGroup.childItems ())
