0e65aeb3573f6327178641e9feb4eef14ac1b485
Gauthier Legrand <gotleg@gmail.com>
2013-03-19 02:38:22 +0100

    Added MinaVibration to the program

--- mipromatrix.cpp
+++ mipromatrix.cpp
@@ -6,6 +6,7 @@
 #include "minarandompixels.h"
 #include "minastars.h"
 #include "minafallingobjects.h"
+#include "minavibration.h"
 
 // Program "Matrix": animation sets of green effects (Matrix-like)
 MiproMatrix::MiproMatrix(Minotor *minotor) :
@@ -25,9 +26,15 @@ MiproMatrix::MiproMatrix(Minotor *minotor) :
 
     MinoAnimationGroup *asg = new MinoAnimationGroup(this);
     MinaStars *as = new MinaStars(asg);
-    as->setColorH(0.4);
-    as->setDensity(6);
+    as->setColorH(0.3);
+    as->setDensity(4);
+    as->setDuration("2");
+    as->setFrequency("1/2");
     asg->addAnimation(as);
+    MinaVibration *av = new MinaVibration(asg);
+    av->setColorH(0.32);
+    av->setSegments(0.4);
+    asg->addAnimation(av);
     this->addAnimationGroup(asg);
 
     MinoAnimationGroup *afog = new MinoAnimationGroup(this);
