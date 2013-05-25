f67a2065c78fc7aacc8f4c0d62bc97bacd99b8c6
Romuald Conty <neomilium@gmail.com>
2013-05-26 01:06:53 +0200

    MiproDebug: create a program with one animation of each available models

--- miprodebug.cpp
+++ miprodebug.cpp
@@ -8,25 +8,36 @@
 #include "minaballs.h"
 #include "minarainbowoil.h"
 
+#include "minopersistentobjectfactory.h"
+
 MiproDebug::MiproDebug(MinoProgramBank *bank) :
     MinoProgram(bank)
 {
     this->setLabel("Debug");
-    for (int i=0;i<35;i++)
+
+//    for (int i=0;i<35;i++)
+//    {
+//        MinoAnimationGroup *asgd = new MinoAnimationGroup(this);
+//        MinaRainbowOil *ad = new MinaRainbowOil(asgd);
+//        ad->setColor(QColor::fromHslF(0.12,0.34,0.56));
+//        ad->setLoopSize("2");
+//        QList<MidiControllableParameter*> pl = ad->findChildren<MidiControllableParameter*>();
+//        for (int n=0;n<3;n++) {
+//            if(n<pl.count())
+//                pl.at(n)->setPreferred(true);
+//        }
+//        asgd->addAnimation(ad);
+//        this->addAnimationGroup(asgd);
+//    }
+
+    QList<MinoAnimationDescription> animations = MinoPersistentObjectFactory::availableAnimationModels();
+    foreach(MinoAnimationDescription animation, animations)
     {
-        MinoAnimationGroup *asgd = new MinoAnimationGroup(this);
-        MinaRainbowOil *ad = new MinaRainbowOil(asgd);
-        ad->setColor(QColor::fromHslF(0.12,0.34,0.56));
-        ad->setLoopSize("2");
-        QList<MidiControllableParameter*> pl = ad->findChildren<MidiControllableParameter*>();
-        for (int n=0;n<3;n++) {
-            if(n<pl.count())
-                pl.at(n)->setPreferred(true);
-        }
-        asgd->addAnimation(ad);
-        this->addAnimationGroup(asgd);
+        MinoAnimationGroup *mag = new MinoAnimationGroup(this);
+        mag->addAnimation(animation.className());
+        addAnimationGroup(mag);
     }
-/*
+    /*
     MinoAnimationGroup *asg = new MinoAnimationGroup(this);
     MinaImage *ai = new MinaImage(asg);
     ai->setColor(QColor::fromHslF(0.32, 1.0, 0.5));
