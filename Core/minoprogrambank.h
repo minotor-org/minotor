6547f552e777ff2d6d4c59e1fa904e55ac5e590e
Romuald Conty <neomilium@gmail.com>
2013-06-18 20:33:15 +0200

    Minotor: add GPL header to *.cpp *.h

--- Core/minoprogrambank.h
+++ Core/minoprogrambank.h
@@ -1,3 +1,23 @@
+/*
+ * Copyright 2012, 2013 Gauthier Legrand
+ * Copyright 2012, 2013 Romuald Conty
+ * 
+ * This file is part of Minotor.
+ * 
+ * Minotor is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Minotor is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Minotor.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef MINOPROGRAMBANK_H
 #define MINOPROGRAMBANK_H
 
